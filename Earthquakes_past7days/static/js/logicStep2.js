c68ee309aea2c60248f664bd1b9d931944960b79
Julie Annis <JKAnnis@Julies-MacBook-Air.local>
2022-03-29 15:44:38 -0500

    Add circleMarker and radius details to the earthquake map.

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -31,6 +31,38 @@ let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/
 
 // Getting geoJSON data.
 d3.json(earthquakeData).then(function(data){
-    // Creating a geoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+    function styleInfo(feature) {
+        return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+        };
+    }
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+        return 1;
+        }
+        return magnitude * 4;
+    }
+// Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+
+    // We turn each feature into a circleMarker on the map.
+    
+        pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+        // Set the style for each circleMarker using styleInfo function
+        style: styleInfo
+    }).addTo(map);
 });
